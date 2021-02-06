e862cbce983f004866bfeceaf5be6be8df53d4e1
Lennart Hansen <lennart@hans1.no>
2021-02-06 15:06:26 +0100

    nt

--- deps/utils.misc.lua
+++ deps/utils.misc.lua
@@ -1,9 +1,8 @@
 local scope = select(2, ...)
 local tinsert = table.insert
 local tremove = table.remove
-local unpack = table.unpack
-
-scope.empty = {}
+local tunpack = table.unpack
+scope.NIL = {}
 
 function scope.next(self, fn, ...)
   if type(fn) == 'function' then
@@ -25,18 +24,31 @@ function scope.splice(self, index, ...)
   return self, ...
 end
 
-function scope.shift(self, ...)
-  for i = 1, select("#", ...) do
-    local arg = select(i, ...)
-    tinsert(self, i, arg)
+function scope.push(self, ...)
+  local l, n = select("#", ...)
+  if self.n then
+    n, self.n = self.n, self.n + l
+  else
+    n = #self
+  end
+  for i = 1, l do
+    self[n+i] = select(i, ...)
   end
   return self
 end
 
-function scope.push(self, ...)
-  for i = 1, select("#", ...) do
-    local arg = select(i, ...)
-    tinsert(self, arg)
+function scope.shift(self, ...)
+  local l, n = select("#", ...)
+  if self.n then
+    n, self.n = self.n, self.n + l
+  else
+    n = #self
+  end
+  for i = n+l, 1, -1 do
+    self[i] = self[i-l]
+  end
+  for i = l, 1, -1 do
+    self[i] = select(i, ...)
   end
   return self
 end
@@ -64,71 +76,37 @@ do
   end
 end
 
----------------------------------------------------------------- CHAIN
-scope.CHAIN = {}
-function scope.CHAIN:__call(...)
-  if #self > 0 then
-    return tremove(self, 1)(self, ...)
-  end
-  return ...
-end
-
 ---------------------------------------------------------------- DB READ/WRITE
 do
-  local function read(tbl, key, ...)
-    if not tbl then return nil end
-    if not key then return tbl end
+  local function read(src, key, ...)
+    if not src then return nil end
+    if not key then return src end
     if type(key) == 'function' then
-      return key(tbl, ...)
+      return key(src, ...)
     end
-    return read(tbl[key], ...)
+    return read(src[key], ...)
   end
   scope.read = read
 end
 
 do
-  local function write(tbl, key, ...)
-    if not tbl then
-      print("#1", tbl, key)
-      return write({}, key, ...)
-    elseif type(key) == 'function' then
-      tbl = key(tbl, ...)
-    elseif select("#", ...) > 0 then
-      print("#2", tbl, key)
-      tbl[key] = write(tbl[key], ...)
-    else
-      print("#3", key)
-      return key
-    end
-    if type(tbl) ~= 'table' then
-      print("#4", tbl, key)
-      return tbl
-    end
-    for _ in pairs(tbl) do
-      return tbl
-    end
-    return nil
-  end
-  scope.write = write
-
-
-
   local poolRelease = scope.poolRelease
-  local poolAcquire, next = scope.poolAcquire, scope.next
-
   local function cleanup(tbl)
-    if type(tbl) ~= 'table' then return end
     for key, val in pairs(tbl) do
-      tbl[key] = cleanup(val)
+      if type(val) == 'table' then
+        cleanup(val)
+      end
+      tbl[key] = nil
     end
     return poolRelease(tbl, nil)
   end
 
-  local function write3(src, arg, ...)
-    if type(arg) == 'function' then
+  local poolAcquire, next = scope.poolAcquire, scope.next
+  local function write(src, key, ...)
+    if type(key) == 'function' then
       local old = src or poolAcquire(nil)
-      local new = next(old, arg, ...)
-      if old ~= new then
+      local new = next(old, key, ...)
+      if old ~= new and type(old) == 'table' then
         cleanup(old)
       end
       if type(new) == 'table' then
@@ -139,121 +117,168 @@ do
       end
       return new
     elseif select("#", ...) == 0 then
-      cleanup(src)
-      return arg
+      if src and type(src) == 'table' then
+        cleanup(src)
+      end
+      return key
     end
-    local old = type(src) == 'table' and src[arg] or nil
-    local new = write3(type(old) == "table" and old or nil, ...)
+    local old = type(src) == 'table' and src[key] or nil
+    local new = write(type(old) == "table" and old or nil, ...)
     if not src then
       if new then
         src = poolAcquire(nil)
-        src[arg] = new
+        src[key] = new
       end
       return src
     end
-    src[arg] = new
+    src[key] = new
     for _ in pairs(src) do
       return src
     end
     return poolRelease(src, nil)
   end
-  scope.write3 = write3
+  scope.write = write
 end
 
---function scope.dbRead(...)
-  --return scope.read(OBroBindsDB, ...)
---end
-
---function scope.dbWrite(...)
-  --OBroBindsDB = scope.write(OBroBindsDB, ...)
---end
+---------------------------------------------------------------- CHAIN
+scope.CHAIN = {}
+function scope.CHAIN:__call(...)
+  if #self > 0 then
+    return tremove(self, 1)(self, ...)
+  end
+  return self, ...
+end
 
 ---------------------------------------------------------------- enqueue, dequeue, dispatch
---function scope.enqueue(key, fn)
-  --if fn and not scope.match(fn, unpack(scope.read(scope, key) or scope.empty)) then
-    --scope.write(scope, key, scope.push, fn)
-    --if #scope.read(scope, key) == 1 and strsub(key, 1, 5) ~= "ADDON" then
-      --scope.root:RegisterEvent(key)
-    --end
-  --end
---end
+do
+  local strsub, NIL = strsub or string.sub, scope.NIL
+  local match, read, write, push = scope.match, scope.read, scope.write, scope.push
+  function scope.enqueue(key, fn)
+    if fn and not match(fn, tunpack(read(scope, key) or NIL)) then
+      write(scope, key, push, fn)
+      if #read(scope, key) == 1 and strsub(key, 1, 5) ~= "ADDON" then
+        scope.root:RegisterEvent(key)
+      end
+    end
+    return fn
+  end
+end
 
---function scope.dequeue(key, fn)
-  --for index, entry in next, scope.read(scope, key) or scope.empty do
-    --if fn == entry then
-      --scope.write(scope, key, scope.splice, index)
-      --break
-    --end
-  --end
-  --if not scope.read(scope, key) and strsub(key, 1, 5) ~= "ADDON" then
-    --scope.root:UnregisterEvent(key)
-  --end
---end
+do
+  local strsub = strsub or string.sub
+  local read, write, splice = scope.read, scope.write, scope.splice
+  function scope.dequeue(key, fn)
+    local subs = read(scope, key)
+    if fn and subs then
+      for index, entry in next, subs do
+        if fn == entry then
+          write(scope, key, splice, index)
+          break
+        end
+      end
+      if not read(scope, key) and strsub(key, 1, 5) ~= "ADDON" then
+        scope.root:UnregisterEvent(key)
+      end
+    end
+  end
+end
 
---function scope:dispatch(key, ...)
-  --local subs = scope.read(scope, key)
-  --if not subs then return end
-  --local chain = scope.poolAcquire(scope.CHAIN, unpack(subs))
-  --return scope.poolRelease(chain, chain(key, ...))
---end
+do
+  local read, push, CHAIN = scope.read, scope.push, scope.CHAIN
+  local poolAcquire, poolRelease = scope.poolAcquire, scope.poolRelease
+  function scope:dispatch(key, ...)
+    local subs = read(scope, key)
+    if subs then
+      local chain = push(poolAcquire(CHAIN), tunpack(subs))
+      return poolRelease(chain(key, ...))
+    end
+    return key, ...
+  end
+end
 
 ------------------------------------------------------------------ STACK
---scope.STACK = {}
---scope.STACK.__index = STACK
---function scope.STACK:__call(...)
-  --return scope.next(scope.push(self, ...), scope.clean, unpack(self))
---end
+scope.STACK = {}
+scope.STACK.__index = scope.STACK
+scope.STACK.n = 0
+do
+  local next, push, clean = scope.next, scope.push, scope. clean
+  function scope.STACK:__call(chain, ...)
+    return select(2, next(push(self, chain, ...), clean, tunpack(self)))(...)
+  end
+end
+do
+  local shift = scope.shift
+  local function call(fn, self, chain, ...)
+    return self, shift(chain, fn), ...
+  end
+  local next = scope.next
+  function scope.STACK.once(self, fn, ...)
+    return call(fn, next(self, ...))
+  end
+  local function skip(self, fn, arg, ...)
+    return next(shift(self, fn, arg), ...)
+  end
+  function scope.STACK.setup(self, fn, ...)
+    return call(fn, next(shift(self, skip, self.setup, fn), ...))
+  end
+  function scope.STACK.clear(self, fn, ...)
+    return next(shift(self, self.setup, fn), ...)
+  end
+  function scope.STACK.both(self, fn, ...)
+    return call(fn, next(shift(self, self.both, fn), ...))
+  end
+  local dequeue, enqueue = scope.dequeue, scope.enqueue
+  function scope.STACK.dequeue(self, event, fn, ...)
+    dequeue(event, fn)
+    return next(shift(self, self.enqueue, event, fn), ...)
+  end
+  function scope.STACK.enqueue(self, event, fn, ...)
+    enqueue(event, fn)
+    return next(shift(self, self.dequeue, event, fn), ...)
+  end
+  local function fold(self, chain, event, ...)
+    dequeue(event, self)
+    return shift(self, self.fold, event), chain, event, ...
+  end
+  function scope.STACK.fold(self, event, ...)
+    if event then
+      enqueue(event, self)
+    end
+    return fold(next(self, ...))
+  end
+  --local mt = {}
+  --function mt:__call(e, ...)
+    --scope.next(SafeUnpack(self))
+    --return e(...)
+  --end
+  --function scope.STACK.apply(...)
+    --return setmetatable(SafePack(...), mt)
+  --end
+end
 
---do
-  --local function fold(key, self, event, ...)
-    --scope.dequeue(event.key, self)
-    --if key then
-      --scope.enqueue(key, self)
-      --scope.shift(self, self.fold, event.key)
-    --end
-    --return event(...)
-  --end
-  --function scope.STACK.fold(self, key, ...)
-    --return fold(key, scope.next(self, ...))
-  --end
---end
---do
-  --local function skip(self, fn, arg, ...)
-    --return scope.next(scope.shift(self, fn, arg), ...)
-  --end
-  --local function call(fn, self, event, ...)
-    --scope.shift(event, fn)
-    --return self, event, ...
-  --end
-  --function scope.STACK.once(self, fn, ...)
-    --return call(fn, scope.next(self, ...))
-  --end
-  --function scope.STACK.setup(self, fn, ...)
-    --return call(fn, scope.next(scope.shift(self, skip, self.call, fn), ...))
-  --end
-  --function scope.STACK.clean(self, fn, ...)
-    --return scope.next(scope.shift(self, self.call, fn), ...)
-  --end
-  --function scope.STACK.both(self, fn, ...)
-    --return call(fn, scope.next(scope.shift(self, self.both, fn), ...))
-  --end
---end
---function scope.STACK.dequeue(self, event, fn, ...)
-  --scope.dequeue(event, fn)
-  --return scope.next(scope.shift(self, self.enqueue, event, fn), ...)
---end
---function scope.STACK.enqueue(self, event, fn, ...)
-  --scope.enqueue(event, fn)
-  --return scope.next(scope.shift(self, self.dequeue, event, fn), ...)
---end
-
-----do
-  ----local mt = {}
-  ----function mt:__call(e, ...)
-    ----scope.next(SafeUnpack(self))
-    ----return e(...)
-  ----end
-  ----function scope.STACK.apply(...)
-    ----return setmetatable(SafePack(...), mt)
-  ----end
-----end
+------------------------------------------------------------------ ACTION
+scope.ACTION = {}
+scope.ACTION.kind  = 1
+scope.ACTION.id    = 2
+scope.ACTION.name  = 3
+scope.ACTION.body  = 3
+scope.ACTION.icon  = 4
+scope.ACTION.lock  = 5
+scope.ACTION.SPELL = 7
+scope.ACTION.MACRO = 7
+scope.ACTION.ITEM  = 7
+scope.ACTION.BLOB  = 7
+do
+  local NIL, ACTION = scope.NIL, scope.ACTION
+  function ACTION:__index(key)
+    if self == NIL then return end
+    local index = ACTION[key]
+    if type(index) ~= 'number' then
+      return index
+    elseif index == 7 then
+      return rawget(self, 1) == key
+    else
+      return rawget(self, index)
+    end
+  end
+end
